40fac7b29dc44d3b96dc47749887c9ccadb007f0
MaximKaratun <37455299+MaximKaratun@users.noreply.github.com>
2018-05-01 21:42:27 +0300

    Update Matrix(3).cpp

--- Matrix-Projects -1-7/Matrix(3).cpp
+++ Matrix-Projects -1-7/Matrix(3).cpp
@@ -8,10 +8,10 @@ D)Найти ср.арифмитическое 2-х значных эл-ов
 E)Найти ср.арифмитическое 3-х значных эл-ов.
 */
 
-#include <iostream>
-#define S 4
-#define C 5
-int  random;
+#include <iostream> //Подключаем библиотеку функции ввода-вывода, работающая в пространстве именования std::;
+#define S 4 //Константное значение S = 4 (Можно и по-другому)
+#define C 5 //Константное значение С = 5 (Можно и по-другому)
+int  random; //Изучаю понемногу указатели и ссылки. А так это функция для рандома.
 int *R = &random;
 
 int main() {
@@ -19,8 +19,8 @@ int main() {
 
 	std::cout << "Matrix string's: " << S << std::endl;
 	std::cout << "Matrix column's: " << C << std::endl;
-	int matrix[S][C];
-
+	int matrix[S][C]; //Создаем 2-хмерный массив. (Хотел одномерный, но он то работает, то не работает).
+//Придаем случайные числа от 999 до 0 нашему 2-хмерному массиву, matrix.
 	for (int i = 0; i < S; i++) {
 		for (int j = 0; j < C; j++) {
 			*R = rand() % 999 + 0;
@@ -31,16 +31,18 @@ int main() {
 
 	std::cout << std::endl;
 
-
+//Выводим на экран по пункту "A)Вывести эту матрицу на экран":
 	for (int i = 0; i < S; i++) {
 		for (int j = 0; j < C; j++) {
 			std::cout << "\t\t" << matrix[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
+	
+	
 	//-------------------------|| B)Посчитать количество 2-х значных элементов || D)Найти ср.арифмитическое 2-х значных эл-ов 
-	double Sumquantity2 = 0;
-	double quantity2 = 0;
+	double Sumquantity2 = 0; //Для суммы.
+	double quantity2 = 0; //Для кол-во 2-хмерных элементов.
 
 
 	for (int i = 0; i < S; i++) {
@@ -52,9 +54,9 @@ int main() {
 		}
 	}
 	std::cout << "\n\n\n\t\tQuantity 2 elements: " << quantity2;
-	std::cout << "\t\tAverage sum 2 elements:" << Sumquantity2 / quantity2 << std::endl;
+	std::cout << "\t\tAverage sum 2 elements:" << Sumquantity2 / quantity2 << std::endl; //Среднее ариф. Сумму делим на кол-во элементов.
 	//-------------------------|| C)Посчитать количество 3-х значных элементов || E)Найти ср.арифмитическое 3-х значных эл-ов
-	int Sumquantity3 = 0;
+	int Sumquantity3 = 0; //Тоже самое.
 	int quantity3 = 0;
 
 	for (int i = 0; i < S; i++) {
@@ -66,7 +68,7 @@ int main() {
 		}
 	}
 	std::cout << "\n\t\tQuantity 3 elements: " << quantity3;
-	std::cout << "\t\tAverage sum 3 elements:" << Sumquantity3 / quantity3 << std::endl;
+	std::cout << "\t\tAverage sum 3 elements:" << Sumquantity3 / quantity3 << std::endl; //Среднее ариф. Сумму делим на кол-во элементов.
 	//-------------------------
 
 
